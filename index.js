127e70f8b8414d48ca4d1500a593118c20c8ba84
Rallge <geraldinelopz@gmail.com>
2021-07-05 05:03:47 +0200

    Creando las funciones cargar, borrar y crear Articulo

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+require("dotenv").config();
 const fetch = require("node-fetch");
 
-( async()=>{
-    const resp = await fetch("http://localhost:3001/articulos");
-    const articulos = await resp.json();
-    console.log(articulos);
-})();
+const urlAPI =process.env.URL_API;
+
+const cargarArticulo = async() => {
+    const resp = await fetch(process.env.URL_API);
+    const articulo = await resp.json();
+    console.log(articulo);
+};
 //node .
+const borrarArticulo = async(id) => {
+    const resp = await fetch(`${urlApi}${id}`,{method:"DELETE"});
+    if(resp.OK){       
+    }
+}
+
+const crearArticulo = async(articulo) => {
+    const resp = await fetch(urlApi,{
+        method: "POST",
+        Headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(articulo)
+    });
+
+const nuevoArticulo = await resp.json();
+
+};
